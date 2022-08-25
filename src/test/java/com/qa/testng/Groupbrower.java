a23275d02d2d075979c7430636d354f634bae7ce
Aniket-123-bit <anisunny782@gmail.com>
2022-08-25 08:45:25 +0530

    first commit

---
+++ src/test/java/com/qa/testng/Groupbrower.java
@@ -0,0 +1,37 @@
+package com.qa.testng;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class Groupbrower {
+	WebDriver driver;
+    
+    String title = "ToolsQA - Demo Website For Automation";
+    
+   
+    @Test
+    public void starting_point(){
+    	System.out.println("This is the starting point of the test");
+    	
+    	System.setProperty("webdriver.chrome.driver","C:\\Users\\ac\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+    	 driver = new ChromeDriver();
+    	 driver.get("https://demoqa.com/");
+    }
+    
+    
+    @Test(groups = { "demo" })	
+    public void checkTitle() {	
+       String testTitle = "Free QA Automation Tools For Everyone";
+       String originalTitle = driver.getTitle();
+      Assert.assertEquals(originalTitle, testTitle);
+    }	
+    
+    @Test(groups = { "demo" })	
+    public void click_element() {	
+       driver.findElement(By.xpath("//*[@id=\"sidebar\"]/aside[1]/ul/li[1]/a")).click();
+        System.out.println("Home Page heading is displayed");	
+    }
+
+}
